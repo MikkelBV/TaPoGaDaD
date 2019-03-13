467686acffc38bd01a57650c214df0225f30826a
Mikkel Brogaard Vittrup <mikkel.b.vittrup@gmail.com>
2019-03-13 20:26:35 +0100

    Add basic monster ai

---
+++ A light in the dark/Assets/Monster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Monster : MonoBehaviour {
+    private NavMeshAgent agent;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = true;
+    }
+
+    public void LightsUpdated(GameObject light1, GameObject light2) {
+        if (!light1.activeSelf && !light2.activeSelf) {
+            agent.SetDestination(transform.position);
+        } else if (!light1.activeSelf) {
+            agent.SetDestination(light2.transform.position);
+        } else if (!light2.activeSelf) {
+            agent.SetDestination(light1.transform.position);
+        } else {
+            var closest = GetClosestLight(light1, light2);
+            agent.SetDestination(closest.transform.position);
+        }
+    }
+
+    GameObject GetClosestLight(GameObject light1, GameObject light2) {
+        var distLight1 = Vector3.Distance(transform.position, light1.transform.position);
+        var distLight2 = Vector3.Distance(transform.position, light2.transform.position);
+
+        return distLight1 < distLight2 ? light1 : light2;
+    }
+
+    void GoToWaypoint(Vector3 waypoint) {
+        agent.destination = waypoint;
+    }
+}
--- A light in the dark/Assets/playerBehavior.cs
+++ A light in the dark/Assets/playerBehavior.cs
@@ -3,110 +3,68 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class playerBehavior : MonoBehaviour
-{
-    float clicked = 0;
-    float clicktime = 0;
-    float clickDelay = 0.8f;
-
+public class playerBehavior : MonoBehaviour {
     public GameObject dropLight1;
     public GameObject dropLight2;
-    private int lightCount;
-    private Vector3 mouseClickPos; 
-    private float distL1, distL2;
+    public Monster monster;
 
+    private Vector3 mouseClickPos;
     private Transform waypoint;
-    private int waypointIndex;
     private NavMeshAgent agent;
-    
-    void Start()
-    {
-        lightCount = 0;
+    private bool ignoreNextLightCollision = false;
+
+    void Start() {
         agent = GetComponent<NavMeshAgent>();
         agent.autoBraking = true;
+
+        dropLight1.SetActive(false);
+        dropLight2.SetActive(false);
     }
 
-    bool DoubleClick()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            clicked++;
-            if (clicked == 1) clicktime = Time.time;
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+                mouseClickPos = hit.point;
+                GoToWaypoint();
+            }
         }
-        if (clicked > 1 && Time.time - clicktime < clickDelay)
-        {
-            clicked = 0;
-            clicktime = 0;
-            return true;
+        if (Input.GetMouseButtonDown(1)) {
+            CheckLight();
         }
-        else if (clicked > 2 || Time.time - clicktime > 1) clicked = 0;
-        return false;
     }
-    
-    void GoToWaypoint(){
+
+    void GoToWaypoint() {
         agent.destination = mouseClickPos;
-        //destPoint = (destPoint+1) % navPoints.Length;
     }
 
-    void CheckLight(){
-        GameObject Light1 = GameObject.Find("Drop Light 1(Clone)");
-        GameObject Light2 = GameObject.Find("Drop Light 2(Clone)");
-
-        if (lightCount < 2){
-            if (!Light1){
-                Instantiate(dropLight1, new Vector3(0 + transform.position.x, 0.20f, 0 + transform.position.z), transform.rotation);
-                lightCount++;
-            }
-            else if (!Light2){
-                Instantiate(dropLight2, new Vector3(0 + transform.position.x, 0.20f, 0 + transform.position.z), transform.rotation);
-                lightCount++;
-            }
+    void CheckLight() {
+        if (!dropLight1.activeSelf) {
+            ActivateLight(dropLight1);
+        } else if (!dropLight2.activeSelf) {
+            ActivateLight(dropLight2);
         }
 
-        if (Light1 && Vector3.Distance(Light1.transform.position, transform.position) < 0.3){
-            Destroy(Light1);
-            lightCount--;
-        }
-        if (Light2 && Vector3.Distance(Light2.transform.position, transform.position) < 0.3){
-            Destroy(Light2);
-            lightCount--;
-        }
-
+        monster.LightsUpdated(dropLight1, dropLight2);
     }
 
-    void Update() // check at clickdelay inden i bevægelsen
-    {
-        
-        
-        if (Input.GetMouseButtonDown(0))
-        {
+    void ActivateLight(GameObject light) {
+        light.transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        light.SetActive(true);
+        ignoreNextLightCollision = true;
+    }
 
-            if (Time.time - clicktime > clickDelay)
-            {
-                RaycastHit hit;
-                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
-                {
-                    mouseClickPos = hit.point;
-                        GoToWaypoint();
-                }
-            }
-
-            if (DoubleClick())
-            {
-                /*
-                lights.Add(
-                    Instantiate(dropLight, new Vector3(0 + transform.position.x, 0.20f, 0 + transform.position.z), transform.rotation) as GameObject
-                );            
-
-                Debug.Log(lights.Count);
-                */
-            }
-        }
-        if (Input.GetMouseButtonDown(1)){
-                CheckLight();
+    void OnTriggerEnter(Collider other) {
+        if (ignoreNextLightCollision) {
+            ignoreNextLightCollision = false;
+            return;
         }
 
+        if (other.gameObject == dropLight1)
+            dropLight1.SetActive(false);
+        else if (other.gameObject == dropLight2)
+            dropLight2.SetActive(false);
 
-
+        monster.LightsUpdated(dropLight1, dropLight2);
     }
 }

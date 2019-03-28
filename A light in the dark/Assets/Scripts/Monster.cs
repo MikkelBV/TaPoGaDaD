cb4a533268b80335229dfd6712c84feff82de100
Mikkel Brogaard Vittrup <mikkel.b.vittrup@gmail.com>
2019-03-28 18:36:34 +0100

    Player now only holds 1 light

--- A light in the dark/Assets/Scripts/Monster.cs
+++ A light in the dark/Assets/Scripts/Monster.cs
@@ -6,12 +6,7 @@ using UnityEngine.AI;
 
 public class Monster : MonoBehaviour {
     private NavMeshAgent agent;
-
-    public GameObject light1;
-    public GameObject light2;
-
-    public playerBehavior player;
-
+    public Player player;
     private int health;
 
     void Start() {
@@ -20,55 +15,26 @@ public class Monster : MonoBehaviour {
         health = 3;
     }
 
-    public void LightsUpdated(GameObject light1, GameObject light2) {
-        if (!light1.activeSelf && !light2.activeSelf) {
-            agent.SetDestination(transform.position);
-        } else if (!light1.activeSelf) {
-            agent.SetDestination(light2.transform.position);
-        } else if (!light2.activeSelf) {
-            agent.SetDestination(light1.transform.position);
+    void Update() {
+        if (Player.LIGHT.activeSelf) {
+            agent.SetDestination(Player.LIGHT.transform.position);
         } else {
-            var closest = GetClosestLight(light1, light2);
-            agent.SetDestination(closest.transform.position);
+            agent.SetDestination(Player.PLAYER.transform.position);
         }
-        
-
     }
 
     //set light attributes on monster collision
-    void OnTriggerEnter(Collider other){
-        
-        if (other.tag == "Light1"){
-            //light1 = other.GetComponent<Light>();
-            other.GetComponent<Light>().color = Color.red;
-            other.GetComponent<Light>().range = 2f;
-            other.GetComponent<Light>().intensity = 1f;
-            player.lightSpawnable = false;
-        }
-        if (other.tag == "Light2" && !player.isPredator){
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.tag == "Light1" && !player.isPredator){
             //light2 = other.GetComponent<Light>();
-            other.GetComponent<Light>().color = Color.red;
-            other.GetComponent<Light>().range = 2;
-            other.GetComponent<Light>().intensity = 1;
-            player.lightSpawnable = false;
-        }
-        if (other.tag == "Light2" && player.isPredator){
+            other.gameObject.gameObject.GetComponent<Light>().color = Color.red;
+            other.gameObject.GetComponent<Light>().range = 2;
+            other.gameObject.GetComponent<Light>().intensity = 1;
+        } else if (other.gameObject.tag == "Light1" && player.isPredator){
             health--;
+            other.gameObject.gameObject.SetActive(false);
             Debug.Log(health);
-            other.gameObject.SetActive(false);
+            Debug.Break();
         }
-
     }
-
-    GameObject GetClosestLight(GameObject light1, GameObject light2) {
-        var distLight1 = Vector3.Distance(transform.position, light1.transform.position);
-        var distLight2 = Vector3.Distance(transform.position, light2.transform.position);
-
-        return distLight1 < distLight2 ? light1 : light2;
-    }
-
-    void GoToWaypoint(Vector3 waypoint) {
-        agent.destination = waypoint;
-    }
-
 }
---
+++ A light in the dark/Assets/Scripts/Player.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour {
+    public static GameObject PLAYER, LIGHT;
+    public GameObject lightObject;
+    public GameObject weapon;
+    public Monster monster;
+    public float lightVelocity = 10f;
+    public float pSpeed = 1f;
+    public float lightSpawnOffset = 1f;
+
+    [HideInInspector] public bool isPredator;
+    [HideInInspector] public bool isLightCollected;
+
+    private Transform waypoint;
+    private NavMeshAgent agent;
+    private int health;
+
+    private Rigidbody rigb;
+    private float modifierZ;
+    private float modifierX;
+
+
+    void Start() {
+        rigb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = true;
+
+        lightObject.SetActive(false);
+        isLightCollected = true;
+        health = 3;
+
+        PLAYER = gameObject;
+        LIGHT = lightObject;
+    }
+
+    void Update() {
+        //WASD Movement
+        if (Input.GetKey(KeyCode.W))
+            rigb.AddForce(0, 0, pSpeed, ForceMode.VelocityChange);
+        if (Input.GetKey(KeyCode.A))
+            rigb.AddForce(-pSpeed, 0, 0, ForceMode.VelocityChange);
+        if (Input.GetKey(KeyCode.S))
+            rigb.AddForce(0, 0, -pSpeed, ForceMode.VelocityChange);
+        if (Input.GetKey(KeyCode.D))
+            rigb.AddForce(pSpeed, 0, 0, ForceMode.VelocityChange);
+
+        if (Input.GetMouseButtonDown(1) && isLightCollected) {
+
+            ActivateLight();
+        }
+
+        if (isPredator){
+            lightObject.GetComponent<Light>().color = new Color(Random.value, Random.value, Random.value);
+        }
+    }
+
+    float getMouseDist(Vector3 position){
+        return Mathf.Abs(transform.position.x - position.x);
+    }
+
+    void ActivateLight() {
+        isLightCollected = false;
+        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = worldMousePos - transform.position;
+
+        direction.y = 0.0f;
+        direction.Normalize();
+        lightObject.GetComponent<Rigidbody>().velocity = lightVelocity * direction;
+
+        lightObject.transform.position = new Vector3(transform.position.x + direction.x * lightSpawnOffset, 0, transform.position.z + direction.z * lightSpawnOffset);
+        lightObject.SetActive(true);
+
+        //set light attributes on activation
+        if(!isPredator){
+            lightObject.GetComponent<Light>().color = Color.white;
+            lightObject.GetComponent<Light>().range = 150f;
+            lightObject.GetComponent<Light>().intensity = 1.75f;
+        }
+        else{
+            lightObject.GetComponent<Light>().color = Color.green;
+            lightObject.GetComponent<Light>().range = 100;
+            lightObject.GetComponent<Light>().intensity = 3f;
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Trap") {
+            Debug.Log("1 damage");
+            TakeDamage(1);
+            return;
+        }
+
+        if (other.gameObject.tag == "Weapon"){
+            isPredator = true;
+            weapon.SetActive(false);
+        }
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.tag == "Monster") {
+            Debug.Log("3 damage");
+            TakeDamage(3);
+        } else if (other.gameObject.tag == "Light1"){
+            lightObject.SetActive(false);
+            isLightCollected = true;
+        }
+    }
+
+    void TakeDamage(int damage) {
+        health -= damage;
+
+        if (health <= 0) {
+            enabled = false;
+            Debug.Log("bitch u ded");
+            Debug.Break();
+        }
+    }
+}

001985add5e7c61fd789a869c89f03e32ff9d888
SimonKoed <31965808+SimonKoed@users.noreply.github.com>
2019-03-29 13:03:44 +0100

    Added basic UI showing player health (More appealing system should be created)

---
+++ A light in the dark/Assets/Scripts/healthBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthBar : MonoBehaviour
+{   // https://www.uihere.com/free-cliparts/vector-graphics-clip-art-pixel-8-bit-color-image-8bit-heart-7119588 - heath png
+    // Start is called before the first frame update
+
+    private int health = 3;
+    public Text healthText;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "Health : " + health;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            health--;
+            print("Health: " + health);
+        }
+
+
+    }
+}

8f190cb4042fd5ff0341910cacb854b22cca7745
SimonKoed <31965808+SimonKoed@users.noreply.github.com>
2019-03-29 13:28:43 +0100

    Added end Screen

---
+++ A light in the dark/Assets/Scripts/endScreen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endScreen : MonoBehaviour
+{
+
+    public GameObject Screen;
+    private bool isDead = false;
+
+
+     void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isDead = true;
+        }
+
+        if (isDead == false)
+        {
+         Screen.gameObject.SetActive(false);
+        } else
+         {
+             Screen.gameObject.SetActive(true);
+         }
+
+      
+    }
+
+
+}

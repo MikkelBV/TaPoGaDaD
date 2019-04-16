126cc0fb84fa4ee2c247e64c214bf6936ad41434
DSEriksen <davideriksen2009@gmail.com>
2019-04-16 11:32:01 +0200

    monster patrol

--- A light in the dark/Assets/Scripts/Monster.cs
+++ A light in the dark/Assets/Scripts/Monster.cs
@@ -4,38 +4,70 @@ using UnityEngine;
 using UnityEngine.AI;
 
 
-public class Monster : MonoBehaviour {
+public class Monster : MonoBehaviour
+{
     public NavMeshAgent agent;
     public Player player;
     private int health;
 
-    void Start() {
+    public Transform[] points;
+    private int destPoint;
+
+    void Start()
+    {
         agent = GetComponent<NavMeshAgent>();
         health = 3;
+
+        GoToNextPoint();
     }
 
-    void Update() {
+    void Update()
+    {
+
+        if (!agent.pathPending && agent.remainingDistance <0.5f){
+            GoToNextPoint();
+        }
+        /*
         if (Player.LIGHT.activeSelf && !player.isPredator ) {
             agent.speed = 1.5f;
             agent.SetDestination(Player.LIGHT.transform.position);
         } else if (!player.isInvisible) {
-            agent.speed = 3f;
+            agent.speed = 1.5f;
             agent.SetDestination(Player.PLAYER.transform.position);
         } else {
             agent.SetDestination(transform.position);
         }
+        */
+    }
+
+    public void GoToNextPoint()
+    {
+        if (points.Length == 0)
+        {
+            return;
+        }
+
+        agent.destination = points[destPoint].position;
+
+        destPoint = (destPoint + 1) % points.Length;
     }
 
     //set light attributes on monster collision
-    void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == "Light1" && !player.isPredator){
+    /*
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Light1" && !player.isPredator)
+        {
             health += 3;
             player.ResetLight();
-        } else if (other.gameObject.tag == "Light1" && player.isPredator){
+        }
+        else if (other.gameObject.tag == "Light1" && player.isPredator)
+        {
             health--;
             other.gameObject.gameObject.SetActive(false);
             Debug.Log(health);
             Debug.Break();
         }
     }
+    */
 }
